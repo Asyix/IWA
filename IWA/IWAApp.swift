67ba2ada58b23014fb8ba8d50ac7dde5140e9375
margotgoudard <goudard.margot@hotmail.com>
2025-03-12 10:40:13 +0100

    Initial Commit

---
+++ IWA/IWAApp.swift
@@ -0,0 +1,17 @@
+//
+//  IWAApp.swift
+//  IWA
+//
+//  Created by etud on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct IWAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
